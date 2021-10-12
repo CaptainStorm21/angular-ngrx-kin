55ef2f09360e7590b6e2de84b69d445f74370a7f
Captain Storm <CaptainStorm21@users.noreply.github.com>
2021-10-11 17:42:06 -0700

    1 - initial setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,26 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-@NgModule({
-  declarations: [
-    AppComponent
-  ],
+import { FormsModule } from '@angular/forms'; import { MatDialogModule } from '@angular/material/dialog';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { AppRoutingModule } from '@app/app-routing.module';
+import { AppComponent } from '@app/app.component';
+
+@NgModule({ declarations: [ AppComponent ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
-export class AppModule { }
+    BrowserAnimationsModule,
+    FormsModule,
+    MatDialogModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatFormFieldModule,
+    MatInputModule],
+  providers: [], bootstrap: [AppComponent]
+}) export class AppModule { }
+
