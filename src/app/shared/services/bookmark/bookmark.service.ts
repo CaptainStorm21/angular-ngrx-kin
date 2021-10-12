f17f736ee34b5ceb71fd3f8f2a882598373f0757
Captain Storm <CaptainStorm21@users.noreply.github.com>
2021-10-11 19:11:40 -0700

    3-52- update bookmark service with function that return 3 things

---
+++ src/app/shared/services/bookmark/bookmark.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+export interface Bookmark{
+  id: number;
+  name: string;
+  url: string;
+  created: Date;
+}
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BookmarkService {
+
+  allBookmarks: Bookmark[];
+  editBookMark: Bookmark;
+  filterText: string;
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  public getAll(): Observable<Bookmark[]>{
+    return this.http.get<Bookmark[]>('api/bookmarks');
+  }
+
+
+  public getById(bookmarkId: number): Observable<Bookmark> {
+    return this.http.get<Bookmark>(`api/bookmarks/${bookmarkId}`);
+  }
+
+  public save(bookmark: Bookmark) {
+    return this.http.post<Bookmark>(`api/bookmarks`, bookmark);
+  }
+
+  public update(bookmark : any): Observable<Bookmark> {
+    return this.http.put<Bookmark>(`api/bookmarks`, bookmark)
+  }
+
+}
