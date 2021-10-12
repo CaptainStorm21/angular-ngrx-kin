41ae6d4c6c1eb53b811cd49ff553f6c1966a2f4c
Captain Storm <CaptainStorm21@users.noreply.github.com>
2021-10-11 20:16:41 -0700

    lazyroute alonw ith everyone else

--- src/app/shared/services/bookmark/bookmark.service.ts
+++ src/app/shared/services/bookmark/bookmark.service.ts
@@ -1,32 +1,35 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { getLocaleDateTimeFormat } from '@angular/common';
 
-export interface Bookmark{
+export interface Bookmark {
   id: number;
   name: string;
   url: string;
   created: Date;
 }
 
-
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class BookmarkService {
-
-  allBookmarks: Bookmark[];
-  editBookMark: Bookmark;
-  filterText: string;
+  allBookmarks: Bookmark[] = [];
+  editBookMark: Bookmark = {
+    id: 0,
+    name: '',
+    url: '',
+    created: new Date(),
+  };
+  filterText: string = '';
 
   constructor(
-    private http: HttpClient
-  ) { }
+    private http: HttpClient,
+  ) {}
 
-  public getAll(): Observable<Bookmark[]>{
+  public getAll(): Observable<Bookmark[]> {
     return this.http.get<Bookmark[]>('api/bookmarks');
   }
-
 
   public getById(bookmarkId: number): Observable<Bookmark> {
     return this.http.get<Bookmark>(`api/bookmarks/${bookmarkId}`);
@@ -36,8 +39,7 @@ export class BookmarkService {
     return this.http.post<Bookmark>(`api/bookmarks`, bookmark);
   }
 
-  public update(bookmark : any): Observable<Bookmark> {
-    return this.http.put<Bookmark>(`api/bookmarks`, bookmark)
+  public update(bookmark: any): Observable<Bookmark> {
+    return this.http.put<Bookmark>(`api/bookmarks`, bookmark);
   }
-
 }
