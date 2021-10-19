6d9fc5d0da34ee6118be4b1b6ff76b2b82898071
Captain Storm <CaptainStorm21@users.noreply.github.com>
2021-10-18 18:37:01 -0700

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [{ path: 'create', loadChildren: () => import('./create/create.module').then(m => m.CreateModule) }, { path: 'list', loadChildren: () => import('./list/list.module').then(m => m.ListModule) }, { path: 'edit/:bookmarkId', loadChildren: () => import('./edit/edit.module').then(m => m.EditModule) }];
+const routes: Routes = [
+  { path: 'create', loadChildren: () => import('./create/create.module').then(m => m.CreateModule) },
+  { path: 'list', loadChildren: () => import('./list/list.module').then(m => m.ListModule) },
+  { path: 'edit/:bookmarkId', loadChildren: () => import('./edit/edit.module').then(m => m.EditModule) },
+  { path: '', redirectTo: '/list', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
